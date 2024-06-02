2f6cda3969c028a2cbb811d545a14362bb965bc3
Piyush <piyushupwar@gmail.com>
2024-06-03 00:14:24 +0530

    notification click handled

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -80,7 +80,22 @@ export const Landing = () => {
       }
     };
 
+    // const urlBase64ToUint8Array = (base64String) => {
+    //   const padding = '='.repeat((4 - base64String.length % 4) % 4);
+    //   const base64 = (base64String + padding).replace(/\-/g, '+').replace(/_/g, '/');
+    //   const rawData = window.atob(base64);
+    //   const outputArray = new Uint8Array(rawData.length);
+
+    //   for (let i = 0; i < rawData.length; ++i) {
+    //     outputArray[i] = rawData.charCodeAt(i);
+    //   }
+    //   return outputArray;
+    // };
+
     const urlBase64ToUint8Array = (base64String) => {
+      if (!base64String) {
+        throw new Error("VAPID public key is not defined");
+      }
       const padding = '='.repeat((4 - base64String.length % 4) % 4);
       const base64 = (base64String + padding).replace(/\-/g, '+').replace(/_/g, '/');
       const rawData = window.atob(base64);
