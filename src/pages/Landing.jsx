129307cd14ce34e5ef0e64bf6802f60104774028
Piyush <piyushupwar@gmail.com>
2024-06-03 00:26:20 +0530

    notification click handled

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -49,10 +49,10 @@ export const Landing = () => {
 
             if (newPermission === "granted") {
               // Permission granted, subscribe to push notifications
-              // const applicationServerKey = urlBase64ToUint8Array(process.env.VAPID_PUBLIC_KEY);
+              const applicationServerKey = urlBase64ToUint8Array(process.env.VAPID_PUBLIC_KEY);
               const subscription = await register.pushManager.subscribe({
                 userVisibleOnly: true,
-                applicationServerKey: process.env.VAPID_PUBLIC_KEY,
+                applicationServerKey,
               });
 
             console.log("Inside function......3");
@@ -87,20 +87,21 @@ export const Landing = () => {
       }
     };
 
-    // const urlBase64ToUint8Array = (base64String) => {
-    //   if (!base64String) {
-    //     throw new Error("VAPID public key is not defined");
-    //   }
-    //   const padding = '='.repeat((4 - base64String.length % 4) % 4);
-    //   const base64 = (base64String + padding).replace(/\-/g, '+').replace(/_/g, '/');
-    //   const rawData = window.atob(base64);
-    //   const outputArray = new Uint8Array(rawData.length);
+    const urlBase64ToUint8Array = (base64String) => {
+      if (!base64String) {
+        // throw new Error("VAPID public key is not defined");
+        console.log("VAPID public key is not defined");
+      }
+      const padding = '='.repeat((4 - base64String.length % 4) % 4);
+      const base64 = (base64String + padding).replace(/\-/g, '+').replace(/_/g, '/');
+      const rawData = window.atob(base64);
+      const outputArray = new Uint8Array(rawData.length);
 
-    //   for (let i = 0; i < rawData.length; ++i) {
-    //     outputArray[i] = rawData.charCodeAt(i);
-    //   }
-    //   return outputArray;
-    // };
+      for (let i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+      }
+      return outputArray;
+    };
 
   const getTopics = async() => {
       try{
